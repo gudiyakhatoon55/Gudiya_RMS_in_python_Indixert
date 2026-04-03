97a4420f46c8be7e46b7f804d3dbdacac80dde41
gudiyakhatoon55 <gudiyakhatoonhtce55@gmail.com>
2026-04-03 12:28:37 +0530

    my project

--- app/billing/bill.py
+++ app/billing/bill.py
@@ -1,39 +1,47 @@
+from app.database.filehandler import FileHandler
 from datetime import datetime
 
+
 class Billing:
+
+    def __init__(self):
+        self.file = FileHandler()
 
     def generate_bill(self, cart):
 
         if not cart:
-            print("No items in cart ❌")
+            print("No items to bill ❌")
             return
 
         print("\n========== BILL ==========")
-        print("Date:", datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-        print("--------------------------")
 
         total = 0
 
         for item in cart:
-            name = item["name"]
-            price = item["price"]
-            qty = item["qty"]
-
-            amount = price * qty
+            amount = item["price"] * item["qty"]
             total += amount
+            print(f"{item['name']} x {item['qty']} = ₹{amount}")
 
-            # print(f"{name} x {qty} = ₹{amount}")
-            print(f"{item['name']} ({item['size']}) x {item['qty']} = ₹{amount}")
-
-        print("--------------------------")
-
-        # 💰 GST Calculation (5%)
         gst = total * 0.05
         grand_total = total + gst
 
+        print("--------------------------")
         print(f"Subtotal: ₹{total}")
         print(f"GST (5%): ₹{gst:.2f}")
         print(f"Grand Total: ₹{grand_total:.2f}")
-
         print("==========================")
-        print("Thank you! Visit again 😊")
+
+        # 🔥 SAVE BILL IN JSON
+        bills = self.file.read_file("bills.json")
+
+        bills.append({
+            "date": str(datetime.now()),
+            "items": cart,
+            "total": total,
+            "gst": gst,
+            "grand_total": grand_total
+        })
+
+        self.file.write_file("bills.json", bills)
+
+        print("Bill saved successfully ✅")
